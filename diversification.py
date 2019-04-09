64fe1d24e32a044d79588c168946b7394c0be146
vigor <vigorakbar@gmail.com>
2019-04-10 03:05:29 +0700

    Finished graph initialization

--- diversification.py
+++ diversification.py
@@ -1,9 +1,7 @@
 import sys
 import networkx as nx
 
-NODE_SIZE = 136
-NDCG_PATH = '/home/vigor/Documents/TA/NDCGs/'
-RANKS_PATH = '/home/vigor/Documents/TA/ranks/'
+GRAPH_PATH = '/home/vigor/Documents/TA/graph/'
 
 def mmr():
   print('using MMR')
@@ -14,16 +12,6 @@ def msd():
 def mpt():
   print('using MPT')
 
-def init_relevance():
-  for i in range(0,136):
-    relFile = open(NDCG_PATH+'NDCG'+str(i+1)+'.txt')
-    lineList = relFile.readlines()
-    relFile.close()
-    G.nodes[i]['relevance'] = float(lineList[-3][22:-1])
-
-def init_similarity():
-  print('test')
-
 if __name__ == "__main__":
   print('starting feature selection using diversification method...')
   arg = 'msd'
@@ -31,9 +19,8 @@ if __name__ == "__main__":
     arg = sys.argv[1]
   except IndexError:
     print('set `msd` as default argument...')
-  G = nx.complete_graph(NODE_SIZE)
-  init_relevance()
-  init_similarity()
+  # read Graph
+  G = nx.read_gexf(GRAPH_PATH)
 
   if (arg == 'mmr'):
     mmr()
---
+++ initializeGraph.py
@@ -0,0 +1,74 @@
+import sys
+import networkx as nx
+import scipy.stats as ss
+import statistics
+
+NODE_SIZE = 136
+NDCG_PATH = '/home/vigor/Documents/TA/NDCGs/'
+RANKS_PATH = '/home/vigor/Documents/TA/ranks/'
+GRAPH_PATH = '/home/vigor/Documents/TA/graph/'
+
+def init_relevance():
+  print('initialize relevance score...')
+  for i in range(136):
+    relFile = open(NDCG_PATH+'NDCG'+str(i+1)+'.txt')
+    lineList = relFile.readlines()
+    relFile.close()
+    G.nodes[i]['relevance'] = float(lineList[-3][22:-1])
+
+def init_ranking():
+  print('initialize ranking test result...')
+  for i in range(136):
+    rankList = []
+    rankSeq = []
+    relFile = open(RANKS_PATH+'rank'+str(i+1)+'.txt')
+    lineList = relFile.readlines()
+    relFile.close()
+    x = 0
+    for j in range(len(lineList)):
+      lineSeq = lineList[j][:-1].split('\t')
+      if int(lineSeq[1]) == 0:
+        if len(rankSeq) > 0:
+          rankList.append(rankSeq)
+        rankSeq = []
+        rankSeq.append(float(lineSeq[2]))
+        x += 1
+      else:
+        rankSeq.append(float(lineSeq[2]))
+        x +=1
+    if len(rankSeq) > 0:
+          rankList.append(rankSeq)
+    for j in range(len(rankList)):
+      rankList[j] = ss.rankdata(rankList[j], method='ordinal')
+    featureRankList.append(rankList)
+    print(i)
+
+def covariance(x,y):
+  return sum((a - statistics.mean(x)) * (b - statistics.mean(y)) for (a,b) in zip(x,y)) / len(x)
+
+def spearman(a, b, i):
+  la = a.tolist()
+  lb = b.tolist()
+  if(len(la) == 1 or len(lb) == 1):
+    return 1
+  return covariance(la,lb)/(statistics.stdev(la)*statistics.stdev(lb))
+
+def avg_spearman(a,b):
+  return statistics.mean([spearman(a[i], b[i], i) for i in range(len(a))])
+
+def init_similarity():
+  print('initialize similarity score...')
+  for i in range(136):
+    print(i)
+    for j in range(i+1, 136):
+      print(str((j/136)*100)+'%')
+      G[i][j]['similarity'] = avg_spearman(featureRankList[i], featureRankList[j])
+
+if __name__ == "__main__":
+  print('initializing graph...')
+  G = nx.complete_graph(NODE_SIZE)
+  featureRankList = []
+  init_relevance()
+  init_ranking()
+  init_similarity()
+  nx.write_gexf(G, GRAPH_PATH)
